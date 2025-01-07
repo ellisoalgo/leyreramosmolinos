99c1ec13eb71d1bc14cda92c6d3704e27dac70d9
ellisoalgo <ellisjwhitney@gmail.com>
2025-01-07 16:59:44 +0100

    contact page

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,6 +3,7 @@ import "../styles/Contacto.scss";
 import Hero from './Hero.jsx'
 import ContactXs from '../images/contact_xs.png'
 import { Link } from 'react-router-dom';
+import '@material/web/all.js';
 
 function Contacto() {
   return (
@@ -15,49 +16,63 @@ function Contacto() {
                 <p className='contact-text_text'>Estoy aquí para escucharte. Si tienes preguntas o necesitas más información, no dudes en escribirme.</p>
                 <form className='contact-text_form'>
                     <div className='contact-text_form-field'>
-                        <input type="text" id="name" name="name" placeholder='Nombre*' required className='contact-text_form-field_input'/>
+                        <md-outlined-text-field type="text" id="name" name="name" required label='Nombre'></md-outlined-text-field>
                     </div>
-                    <div className='contact-text_form-field'>
+                    <div role="radiogroup" aria-labelledby="contact-type" className='contact-text_form-radio'>
                         <label for="contact" className='contact-text_form-field_label'>¿Prefieres que te contacte por teléfono o por correo electrónico?</label>
-                        <input type="radio" id="contact" name="contact" value="email" required className='contact-text_form-field_input'/>
-                        <label for="contact" className='contact-text_form-field_label'>Correo electrónico</label>
-                        <input type="radio" id="contact" name="contact" value="phone" required className='contact-text_form-field_input'/>
-                        <label for="contact" className='contact-text_form-field_label'>Teléfono</label>
-                        <input type="radio" id="contact" name="contact" value="both" required className='contact-text_form-field_input'/>
-                        <label for="contact" className='contact-text_form-field_label'>Por ambos</label>
+                        <div className='contact-text_form-radio_opt'>
+                            <md-radio type="radio" id="contact" name="contact" value="email" aria-label='email' required/>
+                            <label for="contact" className='contact-text_form-field_label'>Correo electrónico</label>
+                        </div>
+                        <div className='contact-text_form-radio_opt'>
+                            <md-radio type="radio" id="contact" name="contact" value="phone" aria-label='phone' required/>
+                            <label for="contact" className='contact-text_form-field_label'>Teléfono</label>
+                        </div>
+                        <div className='contact-text_form-radio_opt'>
+                            <md-radio  type="radio" id="contact" name="contact" value="both" aria-label='both' required/>
+                            <label for="contact" className='contact-text_form-field_label'>Por ambos</label>
+                        </div>
                     </div>
                     <div className='contact-text_form-field'>
-                        <label for="email" className='contact-text_form-field_label'>Escribe tu correo electrónico</label>
-                        <input type="email" id="email" name="email" placeholder="Correo electrónico*" required className='contact-text_form-field_input'/>
+                        <label for="email" className='contact-text_form-field_label'>Escribe tu correo electrónico:</label>
+                        <md-outlined-text-field type="email" id="email" name="email" label='Correo electrónico' required></md-outlined-text-field>
                     </div>
                     <div className='contact-text_form-field'>
-                        <label for="phone" className='contact-text_form-field_label'>Escribe tu número de teléfono</label>
-                        <input type="tel" id="phone" name="phone" placeholder="Teléfono*" required className='contact-text_form-field_input'/>
+                        <label for="phone" className='contact-text_form-field_label'>Escribe tu número de teléfono:</label>
+                        <md-outlined-text-field type="tel" id="phone" name="phone" label='Teléfono' required></md-outlined-text-field>
                     </div>
                     <div className='contact-text_form-field'>
-                        <label for="msj" className='contact-text_form-field_label'>Mensaje</label>
-                        <textarea id="msj" name="msj" placeholder='Mensaje' required className='contact-text_form-field_input'></textarea>
+                        <label for="msj" className='contact-text_form-field_label'>Mensaje:</label>
+                        <md-outlined-text-field type='textarea' id="msj" name="msj" label='Mensaje' required></md-outlined-text-field>
                     </div>
-                    <div className='contact-text_form-field'>
-                        <input type="checkbox" id="check" name="check" value="check" required  className='contact-text_form-field_input'/>
-                        <label for="check" className='contact-text_form-field_label'>He leído, entendido y acepto la <Link to="/terminos-legales-privacidad">política de privacidad</Link></label>
+                    <div className='contact-text_form-check'>
+                        <md-checkbox type="checkbox" id="check" name="check" value="check" required></md-checkbox>
+                        <label for="check" className='contact-text_form-field_label'>He leído, entendido y acepto la <Link to="/terminos-legales-privacidad" className='contact-text_form-field_label-link'>política de privacidad</Link></label>
                     </div>
                     <button type="submit" className='contact-text_form-submit'>Enviar mensaje</button>
                 </form>
             </div>
         </section>
-        <section className='address'>
-            <div className='address_links'>
-                <h2 className='address_links-h2'>Cómo llegar</h2>
-                <p className='address_links-p'>
-                    Carrer de Casp 172, 2º A, 08008 Barcelona, España.
-                    Metro y Rodalies Arc de Triomf.
-                    614 278 073
-                </p>
-                <a href="" className='address_links-link'>Ver en Google Maps</a>
-            </div>
-            <div className='address_map'>
-                {/* embed maps */}
+        <section className='sectAdd'>
+            <div className='sectAdd-address'>
+                <div className='sectAdd-address_map'>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.9475792527232!2d2.1785136756994787!3d41.39694477129869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a2e11321961f%3A0x3828506ad0233f59!2sCarrer%20de%20Casp%2C%20172%2C%202%C2%BA%2C%20A%2C%20L&#39;Eixample%2C%2008013%20Barcelona!5e0!3m2!1sen!2ses!4v1736264321838!5m2!1sen!2ses" width="256" height="256" style={{ border: "0", borderRadius: "16px", width: "100%", height: "100%" }}allowFullScreen=""></iframe>
+                </div>
+                <div className='sectAdd-address_links'>
+                    <h2 className='sectAdd-address_links-h2'>Cómo llegar</h2>
+                    <p className='sectAdd-address_links-p'>
+                        Carrer de Casp 172, 2º A, 08008 Barcelona, España.
+                    </p>
+                    <p className='sectAdd-address_links-p'>
+                        Metro y Rodalies Arc de Triomf.
+                    </p>
+                    <p className='sectAdd-address_links-p'>
+                        614 278 073
+                    </p>
+                    <button className='sectAdd-address_links-btn'>
+                        <a href="" >Ver en Google Maps</a>
+                    </button>
+                </div>
             </div>
         </section>
     </>
