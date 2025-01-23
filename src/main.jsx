f28a64a8deec10f7dd67599469cb27f95d408e18
ellisoalgo <ellisjwhitney@gmail.com>
2025-01-23 13:38:15 +0100

    upload fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import { HashRouter } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { HashRouter } from "react-router-dom"
+import App from "./components/App.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App/>
+      <App />
     </HashRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/leyreramosmolinos/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: "", // Change this to empty string
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
 })
